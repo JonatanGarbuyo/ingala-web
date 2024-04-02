d5012096c07c9a8d503db5c23031eb62dcc2ad2c
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2024-04-02 02:23:05 -0300

    config: fonts with Tailwind definitions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,62 +1,72 @@
-/** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
+/** @type {import('tailwindcss').Config} */
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
 			blue: '#4f7fe5',
+			darkGreen: '#25864D',
+			darkGrey: '#494949',
+			lightGreen: '#74C26C',
+			lightGrey: '#E7E3E2',
 			orange: '#fd8230',
 			pink: '#E6B3B6',
-			lightGrey: '#E7E3E2',
-			darkGreen: '#25864D',
-			lightGreen: '#74C26C',
+			white: '#FFFFFF',
 			yellow: '#EBB93F',
-			darkGrey: '#494949',
 		},
-		fontSize: {
-			title: [
-				'50px',
-				{
-					lineHeight: '60px',
-					letterSpacing: '-0.3px',
-					fontWeight: 600,
-				},
-			],
-			subtitle: [
-				'35px',
-				{
-					lineHeight: '45px',
-					letterSpacing: '-0.3px',
-					fontWeight: 600,
-				},
-			],
-			button: [
-				'16px',
-				{
-					lineHeight: '24px',
-					letterSpacing: '0.5px',
-					fontWeight: 500,
-				},
-			],
-			pl: [
-				'16px',
-				{
-					lineHeight: '22px',
-					fontWeight: 300,
-				},
-			],
-			pm: [
-				'14px',
-				{
-					lineHeight: '20px',
-					fontWeight: 300,
-				},
-			],
-		},
+
 		extend: {
+			maxWidth: {
+				1440: '1440px',
+			},
+			fontSize: {
+				title: [
+					'50px',
+					{
+						lineHeight: '60px',
+						letterSpacing: '-0.3px',
+						fontWeight: 600,
+					},
+				],
+				subtitle: [
+					'35px',
+					{
+						lineHeight: '45px',
+						letterSpacing: '-0.3px',
+						fontWeight: 600,
+					},
+				],
+				button: [
+					'16px',
+					{
+						lineHeight: '24px',
+						letterSpacing: '0.5px',
+						fontWeight: 500,
+					},
+				],
+				pl: [
+					'16px',
+					{
+						lineHeight: '22px',
+						fontWeight: 300,
+					},
+				],
+				pm: [
+					'14px',
+					{
+						lineHeight: '20px',
+						fontWeight: 300,
+					},
+				],
+			},
 			fontFamily: {
-				poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+				'poppins-600': ['Poppins-600', ...defaultTheme.fontFamily.sans],
+				'poppins-700': ['Poppins-700', ...defaultTheme.fontFamily.sans],
+				'roboto-500': ['Roboto-500', ...defaultTheme.fontFamily.sans],
+				'roboto-700': ['Roboto-500', ...defaultTheme.fontFamily.sans],
+				roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+				'roboto-300': ['Roboto-300', ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
