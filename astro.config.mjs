4a1863c00c654a05c70faa63870fff2552c2578b
Jonatan Garbuyo <58077441+JonatanGarbuyo@users.noreply.github.com>
2024-04-02 09:40:50 -0300

    [FEATURE] Layout footer (#2)
    * config; set astro version to 4.5.12
    
    * fix: hot reload
    
    * config: fonts with Tailwind definitions
    
    * config: add svg resources
    
    * feature: add footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+	vite: {
+		server: {
+			// host: '0.0.0.0',
+			// hmr: { clientPort: 3000 },
+			// port: 3000,
+			watch: {
+				usePolling: true,
+				// useFsEvents: true
+			},
+		},
+	},
+	integrations: [tailwind()],
+})
